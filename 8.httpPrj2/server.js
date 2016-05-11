069fb7c3c6ad555990d1053027816c57e08b7a03
fanjianzhou <369019759.com>
2016-05-11 12:32:02 +0800

    分页修改

--- 8.httpPrj2/server.js
+++ 8.httpPrj2/server.js
@@ -22,50 +22,51 @@ var server = http.createServer(function (req, res) {
         })
     }
     else if (urlObjct.pathname == "/getList") {
-        var page=1;
-        var Pagesize=10;
+        var page = 1;
+        var Pagesize = 10;
         //console.log(urlObjct.query.page=="undefined");
-        if(urlObjct.query.page!="undefined"){
-            page=urlObjct.query.page;
+        if (urlObjct.query.page != "undefined") {
+            page = urlObjct.query.page;
         }
-        if(urlObjct.query.Pagesize!="undefined"){
-            Pagesize=urlObjct.query.Pagesize;
+        if (urlObjct.query.Pagesize != "undefined") {
+            Pagesize = urlObjct.query.Pagesize;
         }
         //计算从那条数据开始
-       //当前页1  0   Pagesize*(1-1) -》 Pagesize*(page-1)
-       //当前页2  10  Pagesize*(2-1) -》 Pagesize*(page-1)
-       //当前页3  20  Pagesize*(3-1) -》 Pagesize*(page-1)
-       var start=Pagesize*(page-1);
+        //当前页1  0   Pagesize*(1-1) -》 Pagesize*(page-1)
+        //当前页2  10  Pagesize*(2-1) -》 Pagesize*(page-1)
+        //当前页3  20  Pagesize*(3-1) -》 Pagesize*(page-1)
+        var start = Pagesize * (page - 1);
         //计算从那条数据结束
-       var end= Pagesize*page;
+        var end = Pagesize * page;
 
         //读取数据
-        fs.readFile("./1.txt", function (err,data) {
-            if(err){
+        fs.readFile("./1.txt", function (err, data) {
+            if (err) {
                 res.end("{\"success\":\"no\",\"error\":\"服务器发生错误！请稍后再试\"}");
             }
-            // 20 20 30
-            else{
+
+            else {
                 //将数据转换成对象
-                var obj=JSON.parse(data.toString())
+                var obj = JSON.parse(data.toString())
                 //console.log(obj.result.length,start,end);
-                //判断数据量是否够 当前查询的数据条数大于等于开始条数
-                //如果大于等于那就证明数据够 去集合中截取数据
+                //判断数据量是否够 当前查询的数据条数大于开始条数
+                //如果大于那就证明数据够 去集合中截取数据
                 //如果小于那么说明数据不够  不用去集合中截取数据
-                if(obj.result.length>=start){
+
+                if (obj.result.length > start) {
                     //根据查询条件返回对应的结果
-                    obj.result=obj.result.slice(start,end);
+                    obj.result = obj.result.slice(start, end);
                     //向客户返回当前时间
-                    obj.time=new Date().toLocaleString();
+                    obj.time = new Date().toLocaleString();
                     //向客户返回操作状态
-                    obj.success="ok";
+                    obj.success = "ok";
                     //console.log(JSON.stringify(obj));
                     res.end(JSON.stringify(obj));
                 }
-                else{
-                    var temobj={};
-                    temobj.success="no";
-                    temobj.error="没有数据了！";
+                else {
+                    var temobj = {};
+                    temobj.success = "no";
+                    temobj.error = "没有数据了！";
                     //console.log(JSON.stringify(temobj));
                     res.end(JSON.stringify(temobj));
                 }
@@ -73,6 +74,41 @@ var server = http.createServer(function (req, res) {
 
             }
         })
+    }
+    //删除的路由
+    else if(urlObjct.pathname=="/delete")
+    {
+        //获取要删除的id
+        var id= urlObjct.query.id;
+        //读取数据
+        var obj=JSON.parse(fs.readFileSync("./1.txt","utf8"));
+        //循环每一条数据  找出id相等的然后删除
+        if(id==4){
+            //生成返回给客户端的数据
+            var result={};
+            result.success="no";
+            result.error="编号为4的用户不允许删除！";
+            res.end(JSON.stringify(result));
+            return;
+        }
+        for(var i=0;i<obj.result.length;i++){
+            var current=obj.result[i];
+            if(current.id==id){
+                obj.result.splice(i,1);
+                break;
+            }
+        }
+        //将对象转换成字符串
+        var objstr=JSON.stringify(obj);
+        //将更新数据保存到1.txt文档中
+        fs.writeFileSync("./1.txt",objstr,"utf8");
+
+        //生成返回给客户端的数据
+        var result={};
+        result.success="ok";
+        res.end(JSON.stringify(result));
+
+
     }
 
 })
